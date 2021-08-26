b64b2ba98bbcd5727b0772860d584be1b6e0af42
Stephen Kurshner <skurshner@gmail.com>
2021-08-26 14:08:55 -0700

    refactor to shorten function

--- src/caesar.js
+++ src/caesar.js
@@ -8,42 +8,25 @@ const caesarModule = (function () {
 
   function caesar(input, shift = 0, encode = true) {
     if (shift === 0 || shift < -25 || shift > 25) return false; // return false if no shift, shift is 0, < -25, > 25
-    if (!encode) {
-      // if encode is set to false
-      return input // set output variable
-        .toLowerCase() // set it to lower case
-        .split("") //split into an array
-        .map(letter => letter.charCodeAt(0) - 97) // map letters to corresponding numbers
-        .map(number => {
-          if (number >= 0 && number <= 25) {
-            // if number is between 0 and 25 (lowercase letters)
-            let shifted = number - shift; // change the value of the number according to the shift
-            if (shifted > 25) shifted -= 26; // if shifted value is greater than 25, subtract 26 to wrap it
-            if (shifted < 0) shifted += 26; // if shifted value is less than 0, add 26 to wrap it
-            return shifted; //return the shifted values
-          }
-          return number; // return the same value not between 0 and 25
-        })
-        .map(number => String.fromCharCode(97 + number)) // change the number back to a letter
-        .join(""); // join the array back into a string
-    }
-    // if encode is set to true
-    return input // set output variable
-      .toLowerCase() // set it to lower case
-      .split("") //split into an array
-      .map(letter => letter.charCodeAt(0) - 97) // map letters to corresponding numbers
+    return input // return the output of the inputted string...
+      .toLowerCase() // set to lower case
+      .split("") // split into an array
+      .map(letter => letter.charCodeAt(0) - 97) // with letters mapped to corresponding numbers
       .map(number => {
+        // with shifted numbers mapped
         if (number >= 0 && number <= 25) {
           // if number is between 0 and 25 (lowercase letters)
-          let shifted = number + shift; // change the value of the number according to the shift
-          if (shifted > 25) shifted -= 26; // if shifted value is greater than 25, subtract 26 to wrap it
-          if (shifted < 0) shifted += 26; // if shifted value is less than 0, add 26 to wrap it
-          return shifted; //return the shifted values
+          let shifted; // initialize variable to store shifted number
+          encode ? (shifted = number + shift) : (shifted = number - shift);
+          // based on 'encode,' either subtract or add the shifted value from each number
+          shifted > 25 ? (shifted -= 26) : shifted; // if shifted value is greater than 25, subtract 26 to wrap it
+          shifted < 0 ? (shifted += 26) : shifted; // if shifted value is less than 0, add 26 to wrap it
+          return shifted; //return the shifted value
         }
-        return number; // return the same value not between 0 and 25
+        return number; // return the same value if not between 0 and 25
       })
-      .map(number => String.fromCharCode(97 + number)) // change the number back to a letter
-      .join(""); // join the array back into a string
+      .map(number => String.fromCharCode(97 + number)) // mapped back to letters
+      .join(""); // joined back into a string
   }
 
   return {
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -8,19 +8,19 @@ describe("caesar", () => {
     expect(actual).to.equal(expected);
   });
   it("should return false if there is no shift value", () => {
-    const actual = caesar("a");
+    const actual = caesar("thinkful");
     expect(actual).to.be.false;
   });
   it("should return false if shift value is 0", () => {
-    const actual = caesar("a", 0);
+    const actual = caesar("thinkful", 0);
     expect(actual).to.be.false;
   });
   it("should return false if shift value is less than -25", () => {
-    const actual = caesar("a", -26);
+    const actual = caesar("thinkful", -26);
     expect(actual).to.be.false;
   });
   it("should return false if shift value is greater than 25", () => {
-    const actual = caesar("a", 26);
+    const actual = caesar("a", 99);
     expect(actual).to.be.false;
   });
   it("should return a string with letters shifted by a positive number entered for shift", () => {
@@ -48,12 +48,12 @@ describe("caesar", () => {
     const actual = caesar("abc", -3);
     expect(actual).to.equal(expected);
   });
-  it("should work in reverse when encode is set to false", () => {
+  it("should decode correctly when encode is set to false", () => {
     const expected = "thinkful";
     const actual = caesar("wklqnixo", 3, false);
     expect(actual).to.equal(expected);
   });
-  it("should work in reverse when encode is set to false and still ignore capitals, spaces, and non-letter characters", () => {
+  it("should decode correctly when encode is set to false and still ignore capitals, spaces, and non-letter characters", () => {
     const expected = "this is a secret message!";
     const actual = caesar("BPQA qa I amkzmb umaaiom!", 8, false);
     expect(actual).to.equal(expected);
