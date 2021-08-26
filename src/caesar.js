ae73ee2aee4f88d4b87562b12a1d61ba19c1a85e
Stephen Kurshner <skurshner@gmail.com>
2021-08-26 13:27:13 -0700

    caeser function tested and working as expected

--- src/caesar.js
+++ src/caesar.js
@@ -5,27 +5,50 @@
 
 const caesarModule = (function () {
   // you can add any code you want within this function scope
-  const alphabet = "abcdefghijklmnopqrstuvwxyz"; // create string to store alphabet
-  const alphabetArray = alphabet.split(""); // split alphabet into array to access index
-  //console.log(alphabetArray);
 
   function caesar(input, shift = 0, encode = true) {
     if (shift === 0 || shift < -25 || shift > 25) return false; // return false if no shift, shift is 0, < -25, > 25
-    if (encode) {
-      // if encode is true
-      const inputArray = input.toLowerCase().split(""); // convert input to lower case and split into an array
-      console.log(inputArray);
+    if (!encode) {
+      // if encode is set to false
+      return input // set output variable
+        .toLowerCase() // set it to lower case
+        .split("") //split into an array
+        .map(letter => letter.charCodeAt(0) - 97) // map letters to corresponding numbers
+        .map(number => {
+          if (number >= 0 && number <= 25) {
+            // if number is between 0 and 25 (lowercase letters)
+            let shifted = number - shift; // change the value of the number according to the shift
+            if (shifted > 25) shifted -= 26; // if shifted value is greater than 25, subtract 26 to wrap it
+            if (shifted < 0) shifted += 26; // if shifted value is less than 0, add 26 to wrap it
+            return shifted; //return the shifted values
+          }
+          return number; // return the same value not between 0 and 25
+        })
+        .map(number => String.fromCharCode(97 + number)) // change the number back to a letter
+        .join(""); // join the array back into a string
     }
-
-    // map the array to numbers, alphabet array doesn't include index, ignore (continue)
-    //
+    // if encode is set to true
+    return input // set output variable
+      .toLowerCase() // set it to lower case
+      .split("") //split into an array
+      .map(letter => letter.charCodeAt(0) - 97) // map letters to corresponding numbers
+      .map(number => {
+        if (number >= 0 && number <= 25) {
+          // if number is between 0 and 25 (lowercase letters)
+          let shifted = number + shift; // change the value of the number according to the shift
+          if (shifted > 25) shifted -= 26; // if shifted value is greater than 25, subtract 26 to wrap it
+          if (shifted < 0) shifted += 26; // if shifted value is less than 0, add 26 to wrap it
+          return shifted; //return the shifted values
+        }
+        return number; // return the same value not between 0 and 25
+      })
+      .map(number => String.fromCharCode(97 + number)) // change the number back to a letter
+      .join(""); // join the array back into a string
   }
 
   return {
     caesar,
   };
 })();
-
-caesarModule.caesar("ABC", 1);
 
 module.exports = { caesar: caesarModule.caesar };
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -23,4 +23,39 @@ describe("caesar", () => {
     const actual = caesar("a", 26);
     expect(actual).to.be.false;
   });
+  it("should return a string with letters shifted by a positive number entered for shift", () => {
+    const expected = "wklqnixo";
+    const actual = caesar("thinkful", 3);
+    expect(actual).to.equal(expected);
+  });
+  it("should return a string with letters shifted by a negative number entered for shift", () => {
+    const expected = "qefkhcri";
+    const actual = caesar("thinkful", -3);
+    expect(actual).to.equal(expected);
+  });
+  it("should ignore capital letters, spaces, and non-letter characters", () => {
+    const expected = "bpqa qa i amkzmb umaaiom!";
+    const actual = caesar("This is a secret message!", 8);
+    expect(actual).to.equal(expected);
+  });
+  it("should wrap letters when letters go past the alphabet positively", () => {
+    const expected = "cheud pdjdclqh";
+    const actual = caesar("Zebra Magazine", 3);
+    expect(actual).to.equal(expected);
+  });
+  it("should wrap letters when letters go past the alphabet negatively", () => {
+    const expected = "xyz";
+    const actual = caesar("abc", -3);
+    expect(actual).to.equal(expected);
+  });
+  it("should work in reverse when encode is set to false", () => {
+    const expected = "thinkful";
+    const actual = caesar("wklqnixo", 3, false);
+    expect(actual).to.equal(expected);
+  });
+  it("should work in reverse when encode is set to false and still ignore capitals, spaces, and non-letter characters", () => {
+    const expected = "this is a secret message!";
+    const actual = caesar("BPQA qa I amkzmb umaaiom!", 8, false);
+    expect(actual).to.equal(expected);
+  });
 });
