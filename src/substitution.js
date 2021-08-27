7ddc7b719002b1f39d3c1d1cec954c880f41af81
Stephen Kurshner <skurshner@gmail.com>
2021-08-27 10:46:42 -0700

    added message function and tests working for encoding

--- src/substitution.js
+++ src/substitution.js
@@ -5,9 +5,34 @@
 
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
+  function findDuplicate(array) {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[i] === array[j]) {
+          return true;
+        }
+      }
+    }
+  }
 
   function substitution(input, alphabet, encode = true) {
-    // your solution code here
+    if (alphabet.length !== 26) return false;
+    const alphabetArray = alphabet.split("");
+    if (findDuplicate(alphabetArray)) return false;
+
+    // encode
+    // split input into an array
+    return input
+      .toLowerCase()
+      .split("")
+      .map(letter => letter.charCodeAt(0) - 97)
+      .map(number => {
+        number >= 0 && number <= 25 ? (number = alphabetArray[number]) : (number = String.fromCharCode(97 + number));
+        return number;
+      })
+      .join("");
+    // map to numbers 0-25
+    // map to items at alphabet array's index
   }
 
   return {
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,49 @@
-// Write your tests here!
+const { substitution } = require("../src/substitution");
+const expect = require("chai").expect; // Write your tests here!
+
+describe("substitution", () => {
+  describe("Function Validation", () => {
+    it("should be a function", () => {
+      const expected = "function";
+      const actual = typeof substitution;
+      expect(actual).to.equal(expected);
+    });
+  });
+  describe("Error Handling", () => {
+    it("should return false if alphabet isn't 26 characters", () => {
+      const actual = substitution("thinkful", "short");
+      expect(actual).to.be.false;
+    });
+    it("should return false if alphabet has duplicate characters", () => {
+      const actual = substitution("thinkful", "abcdefghijklmnopqrstuvwxya");
+      expect(actual).to.be.false;
+    });
+  });
+  describe("Encoding", () => {
+    it("should return a string with the correct encoding", () => {
+      const expected = "jrufscpw";
+      const actual = substitution("thinkful", "xoyqmcgrukswaflnthdjpzibev");
+      expect(actual).to.equal(expected);
+    });
+    it("should ignore capital letters", () => {
+      const expected = "elp xhm xf mbymwwmfj dne";
+      const actual = substitution("YOU ARE AN EXCELLENT SPY", "xoyqmcgrukswaflnthdjpzibev");
+      expect(actual).to.equal(expected);
+    });
+    it("should ignore spaces", () => {
+      const expected = "elp xhm xf mbymwwmfj dne";
+      const actual = substitution("you are an excellent spy", "xoyqmcgrukswaflnthdjpzibev");
+      expect(actual).to.equal(expected);
+    });
+    it("should ignore special characters in input string", () => {
+      const expected = "elp xhm xf mbymwwmfj dne!";
+      const actual = substitution("you are an excellent spy!", "xoyqmcgrukswaflnthdjpzibev");
+      expect(actual).to.equal(expected);
+    });
+    it("should encode correctly with special characters in alphabet", () => {
+      const expected = "y&ii$r&";
+      const actual = substitution("message", "$wae&zrdxtfcygvuhbijnokmpl");
+      expect(actual).to.equal(expected);
+    });
+  });
+});
