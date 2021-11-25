96121eaa14aadd360cd45b40e8694666c0cdb71a
Stephen Kurshner <skurshner@gmail.com>
2021-11-24 16:20:09 -0800

    added toggle function

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+const caesarSelect = document.getElementById("select-caesar");
+const polybiusSelect = document.getElementById("select-polybius");
+const substitutionSelect = document.getElementById("select-substitution");
+const caesarBlock = document.getElementById("caesar-block");
+const polybiusBlock = document.getElementById("polybius-block");
+const substitutionBlock = document.getElementById("substitution-block");
+
+caesarSelect.addEventListener("click", () => {
+  if (!caesarSelect.classList.contains("btn-active")) {
+    caesarSelect.classList.add("btn-active");
+  }
+  polybiusSelect.classList.remove("btn-active");
+  substitutionSelect.classList.remove("btn-active");
+
+  caesarBlock.classList.remove("hidden");
+  polybiusBlock.classList.add("hidden");
+  substitutionBlock.classList.add("hidden");
+});
+
+polybiusSelect.addEventListener("click", () => {
+  if (!polybiusSelect.classList.contains("btn-active")) {
+    polybiusSelect.classList.add("btn-active");
+  }
+  caesarSelect.classList.remove("btn-active");
+  substitutionSelect.classList.remove("btn-active");
+
+  caesarBlock.classList.add("hidden");
+  polybiusBlock.classList.remove("hidden");
+  substitutionBlock.classList.add("hidden");
+});
+
+substitutionSelect.addEventListener("click", () => {
+  if (!substitutionSelect.classList.contains("btn-active")) {
+    substitutionSelect.classList.add("btn-active");
+  }
+  caesarSelect.classList.remove("btn-active");
+  polybiusSelect.classList.remove("btn-active");
+
+  caesarBlock.classList.add("hidden");
+  polybiusBlock.classList.add("hidden");
+  substitutionBlock.classList.remove("hidden");
+});
